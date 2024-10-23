af752463771364550f12858694983f71c022c728
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-23 08:53:38 -0400

    Add project files.

---
+++ Topic 9 - Controls, Events, and Properties/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_9___Controls__Events__and_Properties
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
